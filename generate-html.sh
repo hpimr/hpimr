4021a4131a49e90eab6d132da2dcf58b8b1d8879
Kyrylo Yatsenko <hedrok@gmail.com>
2016-09-25 18:40:38 +0300

    Реакція на webhook від github

--- generate-html.sh
+++ generate-html.sh
@@ -2,11 +2,10 @@
 set -e
 
 for f in *.asc; do
-    html=${f/asc/html}
-    phtml="../public_html/$html"
-    if ! [ -e $phtml ] || [ $f -nt $phtml ] ; then
+    html="./public_html/${f/asc/html}"
+    if ! [ -e $html ] || [ $f -nt $html ] ; then
         echo "Updating $f";
-        asciidoc -a lang=uk -a themedir=`pwd`/asciidoc-conf --theme=hpimr $f;
-        mv -v $html ../public_html
+        echo "asciidoc -a lang=uk -a themedir=`pwd`/asciidoc-conf --theme=hpimr -o $html $f";
+        asciidoc -a lang=uk -a themedir=`pwd`/asciidoc-conf --theme=hpimr -o $html $f;
     fi
 done
