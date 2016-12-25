9087bfe9afee543a84c66aa68a2ab08fc9083013
Kyrylo Yatsenko <hedrok@gmail.com>
2016-12-25 12:00:46 +0200

    Поштове повідомлення

--- public_html/report.php
+++ public_html/report.php
@@ -2,14 +2,19 @@
 
 $message = "===================\n";
 $message .= date('%c') . ", ${_SERVER['REMOTE_ADDR']}\n";
-$message .= "page: #${_POST['page']}#\n";
-$message .= "fulltext: #${_POST['fulltext']}#\n";
-$message .= "translation: #${_POST['translation']}#\n";
-$message .= "problem: #${_POST['problem']}#\n";
-$message .= "email: #${_POST['email']}#\n";
-$message .= "name: #${_POST['name']}#\n";
-$message .= "email: #${_POST['email']}#\n";
+$message .= "Сторінка: #${_POST['page']}#\n";
+$message .= "Повний текст: #${_POST['fulltext']}#\n";
+$message .= "Виділине: #${_POST['translation']}#\n";
+$message .= "Опис проблеми: #${_POST['problem']}#\n";
+$message .= "Імʼя: #${_POST['name']}#\n";
+$message .= "e-mail: #${_POST['email']}#\n";
 $message .= "\n";
 
+$to = "hedrok@gmail.com";
+$subject = "[гпімр] Повідомлення про помилку";
+
+$headers = "From: .@гпімр.укр";
+
 file_put_contents("/home/hedro105/reports.txt", $message, FILE_APPEND);
+mail($to, $subject, $message, $headers);
 echo "All ok!\n<br/>";
