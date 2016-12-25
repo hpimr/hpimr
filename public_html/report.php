21464e0b9f7503e8d1361e438ea782d533c25640
Kyrylo Yatsenko <hedrok@gmail.com>
2016-12-25 11:57:24 +0200

    Додав дату й IP

--- public_html/report.php
+++ public_html/report.php
@@ -1,6 +1,7 @@
 <?php
 
 $message = "===================\n";
+$message .= date('%c') . ", ${_SERVER['REMOTE_ADDR']}\n";
 $message .= "page: #${_POST['page']}#\n";
 $message .= "fulltext: #${_POST['fulltext']}#\n";
 $message .= "translation: #${_POST['translation']}#\n";
